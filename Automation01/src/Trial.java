33df7a25dcbed92f77cc7a1544d2d60de1c2bec2
shubhangi-brahamne <shubhangibrahamne23@gmail.com>
2022-06-18 21:40:54 +0530

    Automation Testing

---
+++ Automation01/src/Trial.java
@@ -0,0 +1,32 @@
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("7. If Elase Program");
+		System.out.println(" find grater value");
+		int m=6;
+		int n=10;
+		int o=12;
+		int p=9;
+		int q=144;
+		if(m>n) {if(m>o) {if(m>p) {if(m>q)
+			System.out.println("condition True M is grater");}
+		else {System.out.println("Condition Q is greater");}}
+			else {
+			if(n>o) {if(n>p) {if(n>q)
+			System.out.println("Condition N is greater");
+		}
+		else {if(o>p) {if(o>q) {
+			System.out.println("Condition O is greater");
+		}
+		else { if(p>q) {
+			System.out.println("Condition P is greater");
+		}
+		else {System.out.println("Condition Q is greater");
+		}
+		
+	}
+}
+		}}}}}}
